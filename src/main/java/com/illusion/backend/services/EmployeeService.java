c3e89da79c1e07472ee8b6db834acfba3ddd8377
Eetu <Eetu.Suonpaa@hotmail.com>
2021-01-27 22:44:06 +0200

    Add db initialization schema script to get PSQL working

--- src/main/java/com/illusion/backend/services/EmployeeService.java
+++ src/main/java/com/illusion/backend/services/EmployeeService.java
@@ -3,11 +3,13 @@ package com.illusion.backend.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.illusion.backend.EmployeeNotFoundException;
 import com.illusion.backend.dto.Employee;
 import com.illusion.backend.repositories.EmployeeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmployeeService {
@@ -15,9 +17,14 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Transactional
     public List<Employee> findAll() {
         List<Employee> employees = new ArrayList<>();
-        employeeRepository.findAll().forEach(employees::add);;
+        employeeRepository.findAll().forEach(employees::add);
+
         return employees;
     }
+
+    // @Transactional
+    // public 
 }
